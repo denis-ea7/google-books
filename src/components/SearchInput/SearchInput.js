79d1efbca7b8f7166d572e67d5cec1ed81dc1ffc
denis <tv_bb@mail.ru>
2022-07-11 22:07:45 +0300

    добавил кнопку поиска

--- src/components/SearchInput/SearchInput.js
+++ src/components/SearchInput/SearchInput.js
@@ -3,19 +3,19 @@ import GetBooks from "../../store/GetBooks";
 
 const SearchInput = () => {
     const input = (e) => {
-        if (e.keyCode === 13) {
+        if ((e.keyCode === 13)||(e.target.value=='btn')) {
             GetBooks.getData(e.target.value)
         }
     }
 
     return (
-        <div className='SearchInput'
-        >
+        <div className='SearchInput'>
             <div className="input-group rounded">
                 <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
                        onKeyDown={(e) => input(e)}
                        aria-describedby="search-addon"/>
             </div>
+            <button className='btn' value='btn' onClick={(e)=>input(e)}>Search</button>
         </div>
     );
 };
